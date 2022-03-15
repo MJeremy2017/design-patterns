a5e071327e78400b0a38be7840c1405fdbfe9f96
jeremy.zhang <zhangyue9306@gmail.com>
2022-03-15 11:02:26 +0800

    add fan

--- command-pattern/command.py
+++ command-pattern/command.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod, ABC
-from receiver import Light, GarageDoor
+from receiver import Light, GarageDoor, CeilingFan
 
 
 class Command(ABC):
@@ -7,8 +7,15 @@ class Command(ABC):
     def execute(self):
         pass
 
+    @abstractmethod
+    def undo(self):
+        pass
+
 
 class NilCommand(Command):
+    def undo(self):
+        pass
+
     def execute(self):
         pass
 
@@ -20,6 +27,9 @@ class LightOnCommand(Command):
     def execute(self):
         self.light.on()
 
+    def undo(self):
+        self.light.off()
+
 
 class LightOffCommand(Command):
     def __init__(self, light: Light):
@@ -27,6 +37,9 @@ class LightOffCommand(Command):
 
     def execute(self):
         self.light.off()
+
+    def undo(self):
+        self.light.on()
 
 
 class GarageDoorUpCommand(Command):
@@ -36,6 +49,9 @@ class GarageDoorUpCommand(Command):
     def execute(self):
         self.garage_door.up()
 
+    def undo(self):
+        self.garage_door.down()
+
 
 class GarageDoorDownCommand(Command):
     def __init__(self, garage_door: GarageDoor):
@@ -44,3 +60,45 @@ class GarageDoorDownCommand(Command):
     def execute(self):
         self.garage_door.down()
 
+    def undo(self):
+        self.garage_door.up()
+
+
+class CeilingFanHighCommand(Command):
+    def __init__(self, ceiling_fan: CeilingFan):
+        self.ceiling_fan: CeilingFan = ceiling_fan
+        self.prev_speed: int = ceiling_fan.OFF
+
+    def execute(self):
+        self.prev_speed = self.ceiling_fan.get_speed()
+        self.ceiling_fan.high()
+
+    def undo(self):
+        if self.prev_speed == CeilingFan.HIGH:
+            self.ceiling_fan.high()
+        elif self.prev_speed == CeilingFan.MEDIUM:
+            self.ceiling_fan.medium()
+        elif self.prev_speed == CeilingFan.LOW:
+            self.ceiling_fan.low()
+        else:
+            self.ceiling_fan.off()
+
+
+class CeilingFanOffCommand(Command):
+    def __init__(self, ceiling_fan: CeilingFan):
+        self.ceiling_fan: CeilingFan = ceiling_fan
+        self.prev_speed: int = ceiling_fan.OFF
+
+    def execute(self):
+        self.prev_speed = self.ceiling_fan.get_speed()
+        self.ceiling_fan.off()
+
+    def undo(self):
+        if self.prev_speed == CeilingFan.HIGH:
+            self.ceiling_fan.high()
+        elif self.prev_speed == CeilingFan.MEDIUM:
+            self.ceiling_fan.medium()
+        elif self.prev_speed == CeilingFan.LOW:
+            self.ceiling_fan.low()
+        else:
+            self.ceiling_fan.off()
--- command-pattern/receiver.py
+++ command-pattern/receiver.py
@@ -18,3 +18,37 @@ class GarageDoor:
 
     def down(self):
         print(self.name, "garage door is down!")
+
+
+class CeilingFan:
+    OFF = 0
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+    def __init__(self, name: str):
+        self.name: str = name
+        self.speed = self.OFF
+
+    def off(self):
+        self.speed = self.OFF
+        print("speed is off")
+
+    def low(self):
+        self.speed = self.LOW
+        print("speed is low")
+
+    def medium(self):
+        self.speed = self.MEDIUM
+        print("speed is medium")
+
+    def high(self):
+        self.speed = self.HIGH
+        print("speed is high")
+
+    def get_speed(self):
+        return self.speed
+
+
+
+
