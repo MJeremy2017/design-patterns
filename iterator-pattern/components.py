a15f0f3920708997bc496a023f762bcb4ebe713f
jeremy.zhang <zhangyue9306@gmail.com>
2022-03-30 19:15:41 +0800

    add componets pattern

---
+++ iterator-pattern/components.py
@@ -0,0 +1,21 @@
+class MenuComponent:
+    def add(self, menu_component):
+        raise NotImplementedError
+
+    def remove(self, menu_component):
+        raise NotImplementedError
+
+    def get_child(self, index: int):
+        raise NotImplementedError
+
+    def get_name(self):
+        raise NotImplementedError
+
+    def get_price(self):
+        raise NotImplementedError
+
+    def is_vegan(self):
+        raise NotImplementedError
+
+    def print_menu(self):
+        raise NotImplementedError
