43daddd89bd8658604411665a55b8b79b4e7109c
jeremy.zhang <zhangyue9306@gmail.com>
2022-04-07 11:05:51 +0800

    add winner state and refill action

--- state-pattern/state_gumball_machine.py
+++ state-pattern/state_gumball_machine.py
@@ -1,3 +1,6 @@
+import random
+
+
 class StateGumballMachine:
     def __init__(self, num_gumballs):
         self.count = num_gumballs
@@ -5,6 +8,7 @@ class StateGumballMachine:
         self.sold_out_state = SoldOutState(self)
         self.has_quarter_state = HasQuarterState(self)
         self.sold_state = SoldState(self)
+        self.winner_state = WinnerState(self)
         if num_gumballs > 0:
             self.state: State = self.no_quarter_state
         else:
@@ -24,6 +28,12 @@ class StateGumballMachine:
     def set_state(self, state):
         self.state = state
 
+    def refill(self, num_gumballs):
+        self.state.refill(num_gumballs)
+
+    def __str__(self):
+        return f"State: {self.state} | Gumball left: {self.count}"
+
 
 class State:
     def insert_quarter(self):
@@ -37,6 +47,12 @@ class State:
         raise NotImplementedError
 
     def dispense(self):
+        raise NotImplementedError
+
+    def refill(self, num_gumballs):
+        raise NotImplementedError
+
+    def __str__(self):
         raise NotImplementedError
 
 
@@ -57,6 +73,12 @@ class NoQuarterState(State):
     def dispense(self):
         print("You need to pay first")
 
+    def __str__(self):
+        return "No Quarter State"
+
+    def refill(self, num_gumballs):
+        print("There is still gumballs left, you can't refill")
+
 
 class SoldOutState(State):
     def __init__(self, gumball_machine: StateGumballMachine):
@@ -74,6 +96,14 @@ class SoldOutState(State):
     def dispense(self):
         print("No gumball dispensed")
 
+    def refill(self, num_gumballs):
+        self.gumball_machine.count = num_gumballs
+        print(f"Refilled {num_gumballs} gumballs")
+        self.gumball_machine.set_state(self.gumball_machine.no_quarter_state)
+
+    def __str__(self):
+        return "Sold out State"
+
 
 class HasQuarterState(State):
     def __init__(self, gumball_machine: StateGumballMachine):
@@ -88,11 +118,21 @@ class HasQuarterState(State):
 
     def turn_crank(self):
         print("You turned...")
-        self.gumball_machine.set_state(self.gumball_machine.sold_state)
+        lucky_number = random.randint(1, 10)
+        if lucky_number <= 1 and self.gumball_machine.count > 1:
+            self.gumball_machine.set_state(self.gumball_machine.winner_state)
+        else:
+            self.gumball_machine.set_state(self.gumball_machine.sold_state)
 
     def dispense(self):
         # inappropriate method for the state
         print("No gumball dispensed")
+
+    def __str__(self):
+        return "Has Quarter State"
+
+    def refill(self, num_gumballs):
+        print("There is still gumballs left, you can't refill")
 
 
 class SoldState(State):
@@ -116,3 +156,45 @@ class SoldState(State):
             self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
         else:
             self.gumball_machine.set_state(self.gumball_machine.no_quarter_state)
+
+    def __str__(self):
+        return "Sold State"
+
+    def refill(self, num_gumballs):
+        print("There is still gumballs left, you can't refill")
+
+
+class WinnerState(State):
+    def __init__(self, gumball_machine: StateGumballMachine):
+        self.gumball_machine = gumball_machine
+
+    def insert_quarter(self):
+        print("You can't insert quarter")
+
+    def eject_quarter(self):
+        print("You can't eject quarter")
+
+    def turn_crank(self):
+        print("You can't turn crank")
+
+    def dispense(self):
+        print("Congrats! You're a winner")
+        print("A gumball comes rolling out the slot")
+        self.gumball_machine.count -= 1
+        if self.gumball_machine.count == 0:
+            print("Oops, out of gumballs!")
+            self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
+        else:
+            print("A gumball comes rolling out the slot")
+            self.gumball_machine.count -= 1
+            if self.gumball_machine.count == 0:
+                print("Oops, out of gumballs!")
+                self.gumball_machine.set_state(self.gumball_machine.sold_out_state)
+            else:
+                self.gumball_machine.set_state(self.gumball_machine.no_quarter_state)
+
+    def __str__(self):
+        return "No Quarter State"
+
+    def refill(self, num_gumballs):
+        print("There is still gumballs left, you can't refill")
